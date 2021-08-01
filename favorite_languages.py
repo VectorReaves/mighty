af10ebfb9ec8d11ab762747a60d90ea86f725b34
VectorReaves <88208618+VectorReaves@users.noreply.github.com>
2021-08-01 19:53:31 -0400

    Update favorite_languages.py

--- favorite_languages.py
+++ favorite_languages.py
@@ -2,5 +2,7 @@ favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
     'bob': 'java',
-    'zach': 'ruby'
+    'zach': 'ruby',
 }
+language = favorite_languages['sarah'].title()
+print(f"{'sarah'.title()}'s favorite language is {language}.")
